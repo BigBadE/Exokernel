34013fd17e610799b91b56fb02b5a987a55277d3
bigbade <bigbade333@gmail.com>
2022-12-14 14:45:29 -0600

    Worked on fs

--- kernel/arch/x86_64/bootloader/bios/stage-2/src/main.rs
+++ kernel/arch/x86_64/bootloader/bios/stage-2/src/main.rs
@@ -2,10 +2,17 @@
 #![no_main]
 #![feature(lang_items)]
 use core::arch::asm;
+use crate::dap::DAP;
+
+mod dap;
 
 #[no_mangle]
 pub extern "C" fn second_stage() {
     print("Testing!");
+
+    let buffer = [0; 512*8];
+    let packet = DAP::new(8, buffer as *const u8, 1);
+
 }
 
 pub fn print(message: &'static str) {
